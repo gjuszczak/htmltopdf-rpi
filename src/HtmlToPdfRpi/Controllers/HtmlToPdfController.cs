ccd564e7d70da77cce40faed5d3f5bac87433d7e
Grzegorz Juszczak <grzegorz.juszczak@outlook.com>
2021-02-14 12:43:38 +0100

    Add project files.

---
+++ src/HtmlToPdfRpi/Controllers/HtmlToPdfController.cs
@@ -0,0 +1,26 @@
+﻿using HtmlToPdfRpi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HtmlToPdfRpi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HtmlToPdfController : ControllerBase
+    {
+        private readonly IWkHtmlToPdfService _htmlToPdf;
+
+        public HtmlToPdfController(IWkHtmlToPdfService htmlToPdf)
+        {
+            _htmlToPdf = htmlToPdf;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> FromUrl([FromQuery] string url, CancellationToken cancellationToken)
+        {
+            var stream = await _htmlToPdf.FromUrlAsync(url, cancellationToken);
+            return File(stream, "application/pdf");
+        }
+    }
+}
---
+++ src/HtmlToPdfRpi/Services/WkHtmlToPdfService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HtmlToPdfRpi.Services
+{
+    public interface IWkHtmlToPdfService
+    {
+        Task<Stream> FromUrlAsync(string url, CancellationToken cancellationToken = default);
+    }
+
+    public class WkHtmlToPdfService : IWkHtmlToPdfService
+    {
+        public async Task<Stream> FromUrlAsync(string url, CancellationToken cancellationToken)
+        {
+            // need to use tempfile because of bug https://github.com/wkhtmltopdf/wkhtmltopdf/issues/3119
+            var tempFilePath = $"{Path.GetTempPath()}{Guid.NewGuid()}.pdf";
+            var procInfo = new ProcessStartInfo
+            {
+                FileName = "/usr/local/bin/wkhtmltopdf",
+                Arguments = $"-q {url} {tempFilePath}",
+                UseShellExecute = false,
+                RedirectStandardError = true,
+            };
+            using var proc = Process.Start(procInfo);
+            using var msErr = new MemoryStream();
+            using var msErrReader = new StreamReader(msErr);
+            try
+            {
+                await proc.StandardError.BaseStream.CopyToAsync(msErr, cancellationToken);
+                msErr.Seek(0, SeekOrigin.Begin);
+                await proc.WaitForExitAsync(cancellationToken);
+            }
+            finally
+            {
+                if ((proc?.HasExited ?? true) == false)
+                {
+                    proc.Kill();
+                }
+            }
+
+            var msOut = new MemoryStream();
+            using (var fs = File.OpenRead(tempFilePath))
+            {
+                await fs.CopyToAsync(msOut, cancellationToken);
+            }
+            File.Delete(tempFilePath);
+            msOut.Seek(0, SeekOrigin.Begin);
+            if (msOut.Length == 0)
+            {
+                throw new Exception(msErrReader.ReadToEnd());
+            }
+            return msOut;
+        }
+    }
+}
